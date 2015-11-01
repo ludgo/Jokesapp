283598dae0957f4d186766021ce52e15b4895512
ludgo <ludgogit@gmail.com>
2015-11-01 19:46:38 +0100

    java library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,11 +1,13 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+
+import com.udacity.gradle.jokes.Joker;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -40,7 +42,9 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view){
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+
+        Joker myJoker = new Joker();
+        Toast.makeText(this, myJoker.getJoke(), Toast.LENGTH_LONG).show();
     }
 
 
